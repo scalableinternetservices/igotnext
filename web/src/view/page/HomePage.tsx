7ff8bbade601b60a94c0b8687b4fcf5b774658a5
refinedwords <aaronpphilip@gmail.com>
2020-11-04 12:07:03 -0800

    fix bug

--- web/src/view/page/HomePage.tsx
+++ web/src/view/page/HomePage.tsx
@@ -3,7 +3,7 @@ import * as React from 'react'
 import { AppRouteParams } from '../nav/route'
 import { Parks } from '../playground/Parks'
 import { Page } from './Page'
-npm
+
 interface HomePageProps extends RouteComponentProps, AppRouteParams {}
 
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
