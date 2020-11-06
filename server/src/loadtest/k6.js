e5c73fcf57aedc49a2688665489d343b2bada565
Ryan Yang <Ryanyang99@hotmail.com>
2020-11-06 14:51:56 -0800

    add inital k6 setup

---
+++ server/src/loadtest/k6.js
@@ -0,0 +1,12 @@
+/* eslint-disable prettier/prettier */
+
+import http from 'k6/http';
+import { sleep } from 'k6';
+
+export default function () {
+  http.get('http://test.k6.io');
+  sleep(1);
+}
+
+// to run just use k6 run {this file name}
+// follow install instructions on this website https://k6.io/docs/getting-started/running-k6
