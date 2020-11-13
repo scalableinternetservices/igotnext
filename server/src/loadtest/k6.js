dca9cbd24cb401454a553772fe4d09ebb54e736a
Cheng Peng <c2peng@ucla.edu>
2020-11-13 14:08:55 -0800

    adding query loadtesting

--- server/src/loadtest/k6.js
+++ server/src/loadtest/k6.js
@@ -1,31 +1,80 @@
 /* eslint-disable prettier/prettier */
-import http from 'k6/http';
-import { sleep } from 'k6';
-import {parseHTML} from "k6/html";
+import http from 'k6/http'
 
-// export let options = {
-//   vus: 10,
-//   duration: '30s',
-// };
+export let options = {
+  vus: 1000,
+  duration: '10s',
+}
 
 export default function () {
-  let res = http.get('http://localhost:3000/app/index');
-  // Now, submit form setting/overriding some fields of the form
-  res = res.submitForm({
-    formSelector: 'form',
-    fields: { latitude: 33, longitude: -117 , nickname: 'sean' },
-  });
-  sleep(2);
+  const headers = {
+    'Content-Type': 'application/json', //$court_id: Int!, $nickname: String
+  }
+  /*
+  const resp = http.post(
+    //AddToCourt($court_id: Int!, $nickname: String) {addUserToCourt(courtID: $court_id, nickname: $nickname)}
+    'http://localhost:3000/graphql',
+    '{"operationName":"AddToCourt","variables":{"court_id":1,"nickname":"tian"},"query":"  mutation AddToCourt($court_id: Int!, $nickname: String) {addUserToCourt(courtID: $court_id, nickname: $nickname)}"}',
+    {
+      headers,
+    }
+  )
+  */
 
-  console.log(res.body)
-  const doc = parseHTML(res.body);
-  const sel = doc.find('div').children();
+  /*
+  let query = `
+query parkQ{
+  park(latitude: 33, longitude: -117){
+    parkID
+    longitude
+    latitude
+    parkName
+    courts{
+      courtID
+    }
+  }
+}
+  `
+  */
 
-  doc.find("div").children().toArray().forEach(function (item) {
-      // console.log(item.get(0).innerHTML());
-      // make http gets for it
-      // or added them to an array and make one batch request
-   });
+  /*
+  let query = `
+  query Games{
+  allGames{
+   matchID
+    status
+    court{
+      courtID
+    }
+    roster
+
+  }
+}
+  `
+  */
+
+
+  /*
+  let query = `
+  query Parkind{
+  parkind(park_id:1){
+   parkName
+  }
+}
+`
+*/
+
+ let query = `
+ query courtKind{
+  courtind(court_ID:1){
+    courtID
+    courtName
+    lobby
+  }
+}
+ `
+
+  const Query = http.post('http://localhost:3000/graphql', JSON.stringify({ query }), { headers })
 }
 
 // to run just use k6 run {this file name}
